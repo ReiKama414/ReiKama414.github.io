86f583e7329af10d9324c8e9ce985fbffd27894d
Kama <milu77704@gmail.com>
2023-02-01 11:58:17 +0800

    20230201_3

--- dist/_nuxt/static/1675223029/tags/typeweb/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675223029",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Ftypeweb",config:{mdAnchorLevel:[2,3],_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"45371e1f"}},__i18n:{langs:{}}}}(null));
